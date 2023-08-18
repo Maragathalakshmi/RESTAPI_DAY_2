ebe145f3ded23e06b03151a80b27ca58c189737d
Maragathalakshmi <108513716+Maragathalakshmi@users.noreply.github.com>
2023-08-18 19:17:27 +0530

    Update Controller.java

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,5 +1,17 @@
 package com.example.demo.controller;
 
-public class Controller {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class Controller 
+{
+	@Value("${var}")
+	private String name;
+   @GetMapping("/GET")
+   public String getName()
+   {
+	   return "Hello "+name;
+   }
 }
